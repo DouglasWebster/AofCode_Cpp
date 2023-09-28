c1a253c5b60a2ab5ac5badca05fa72ff093ac70d
douglas <douglas.webster2107@gmail.com>
2023-09-28 16:13:57 +0100

    Day01 files regenerated after change to .in files.

---
+++ src/campaigns/2022/day01/2022_day01.cpp
@@ -0,0 +1,10 @@
+#include "2022_day01.hpp"
+
+std::vector<int> collate_calories(const ElvenPacks & elven_packs)
+{
+  std::vector<int> elves_calories{};
+
+  if (elven_packs.empty()) { return elves_calories; }
+
+  return elves_calories;
+}
--- src/campaigns/2022/day01/2022_day01.hpp
+++ src/campaigns/2022/day01/2022_day01.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 namespace AofCode_Cpp::cmake {
     inline constexpr std::string_view campaign_year = "2022";
-    inline constexpr std::string_view campaign_day = "day01";
+    inline constexpr std::string_view campaign_day = "2022";
 } //namespace AofCode::cmake
 
+using ElvenPacks = std::vector<std::vector<int>>;
+
+std::vector<int> collate_calories(const ElvenPacks & elven_packs);
