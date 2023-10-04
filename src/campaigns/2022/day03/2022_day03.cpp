d7fe5a984ee947e16b24216d2ce8d12f8edd5d1e
douglas <douglas.webster2107@gmail.com>
2023-10-04 11:24:44 +0100

    Initialised 2022 day 03 puzzle solution

---
+++ src/campaigns/2022/day03/2022_day03.cpp
@@ -0,0 +1,1 @@
+#include "2022_day03.hpp"
---
+++ test/campaigns/2022/day03/day03_test.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_container_properties.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
+
+#include <iostream>
+
+#include <2022_day03.hpp>
+
+// start with a test case that does nothing but fail.
+
+TEST_CASE(" 2022 day03 testing not started", "[day03]")
+{
+    REQUIRE( false );
+}
