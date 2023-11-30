372638517845f36a1250a2a72fc35d8a05fdaf5e
DouglasWebster <douglas.webster2107@gmail.com>
2023-11-30 19:08:21 +0000

    set up to start AoC year 2023

---
+++ src/campaigns/2023/day01/2023_day01.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+#include <AofCode_Cpp/aoc_library.hpp>
+
+namespace AofCode_Cpp::cmake {
+    inline constexpr std::string_view campaign_year = "2023";
+    inline constexpr std::string_view campaign_day = "day01";
+} //namespace AofCode::cmake
+
