50592eab6c078d8faf48390388fb5689e0b1ac43
DouglasWebster <douglas.webster2107@gmail.com>
2023-12-21 07:46:00 +0000

    Removed unneeded from part 1

--- src/campaigns/2023/day18/2023_day18.hpp
+++ src/campaigns/2023/day18/2023_day18.hpp
@@ -24,30 +24,7 @@ struct Trench {
 
 using DigPlan = std::vector<Trench>;
 
-using Excavation = std::vector<std::vector<char>>;
-
-// The extent left and right of up and down from the start cube
-using Extent = std::pair<size_t, size_t>;
-
-// the Up-Down and Left-Right extents
-using Extents = std::pair<Extent, Extent>;
-
 DigPlan create_plan(const AoCLib::line_data &puzzle_data);
-
-Extents calc_dig_extents(const DigPlan &dig_plan);
-
-Excavation dig_ditches(const DigPlan &dig_plan, const Extents &extents);
-
-void draw_excavation(const Excavation &excavation);
-
-
-void create_lagoon(Excavation &excavation);
-
-int count_lagoon_size(const Excavation &excavation);
-
-void print_excavation(const Excavation &excavation, const std::string &file_name);
-
-void mark_external(Excavation &excavation);
 
 using CoOrdinate = std::pair<double, double>;
 using Vertices = std::vector<CoOrdinate>;
